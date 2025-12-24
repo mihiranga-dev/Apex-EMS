a984ae9202e5387a971a0d4ccda03dec5ff58e31
mihiranga-dev <mihiranga.dev@gmail.com>
2025-12-24 12:11:58 +0530

    Completed MongoDB Data Layer

---
+++ ems-backend/src/main/java/com/abccompany/ems_backend/model/mongo/Attendance.java
@@ -0,0 +1,23 @@
+package com.abccompany.ems_backend.model.mongo;
+
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "attendance_logs")
+@Data
+public class Attendance {
+    @Id
+    private String id;
+
+    private Long employeeId; // This links with the oracle Employee Table
+
+    private LocalDateTime checkInTime;
+    private LocalDateTime checkOutTime;
+
+    private String status; // 'PRESENT' , 'LATE' , 'ON_LEAVE'
+
+    private String ipAddress; // can track which network used
+}
---
+++ ems-backend/src/main/java/com/abccompany/ems_backend/repository/mongo/AttendanceRepository.java
@@ -0,0 +1,15 @@
+package com.abccompany.ems_backend.repository.mongo;
+
+import com.abccompany.ems_backend.model.mongo.Attendance;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AttendanceRepository extends MongoRepository<Attendance, String> {
+
+    // Custom method to find all attendance records for a specific employee from Oracle
+    List<Attendance> findByEmployeeId(Long employeeId);
+
+}
