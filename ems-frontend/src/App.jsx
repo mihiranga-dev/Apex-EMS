f8a9bac32956d35c75f545e617b9116c86b53a46
mihiranga-dev <mihiranga.dev@gmail.com>
2026-02-11 20:56:22 +0530

    feat: Add demo header,main and footer section

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+function Header() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <header style={{ backgroundColor: '#f4f4f4', padding: '10px' }}>
+      <h2 style={{ color: '#000000' }}>Apex Management System</h2>
+    </header>
+  );
 }
 
-export default App
+function Footer() {
+  return (
+    <div>
+      <p>Mihiranga Dev</p>
+      <p>© 2026 Apex-EMS Portal</p>
+    </div>
+  );
+}
+
+
+function App() {
+
+  return (
+    <div>
+      <Header/>
+
+      <main>
+        <h3>Dashboard</h3>
+        <p>Hello, Admin. Welcome back.</p>
+      </main>
+
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
